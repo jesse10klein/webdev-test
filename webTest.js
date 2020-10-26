26af505b64a1de3747ce16cea12a73ea72d1ca44
Jesse Klein <jesse10klein@gmail.com>
2020-10-26 15:16:20 +1000

    Adjacency matrix for graph being created

--- webTest.js
+++ webTest.js
@@ -1,22 +1,83 @@
 
 const fs = require('fs');
 
+let adjMatrix = [];
+let loaded = false;
+
+/*
+  For this problem, I'm going to assume that the number of inputs
+  are relatively small, this way we can use an adjacency matrix
+  to represent the directional graph
+*/
+
+
 function parseFile(fileName) {
 
   fs.readFile(`./${fileName}`, 'utf8', (err, data) => {
     if (err) return console.log(err);
-    console.log(data);
+
+    //Get a list similar to the inputs, then create matrix
+    const parts = data.split('\r\n');
+
+    //We also need to get the distinct count to know how big to
+    //make the matrix
+    let unique = [];
+    for (let i = 0; i < parts.length; i++) {
+      if (!unique.includes(parts[i].charAt(0))) {
+        unique.push(parts[i].charAt(0));
+      }
+    }
+
+    createAdjacencyMatrix(parts, unique.length);
   })
 
 }
 
-parseFile('test.txt');
+function createAdjacencyMatrix(parts, distinctCount) {
+  console.log("ET")
+  //Start off by making a matrix n*n initialised to -1
+  for (let i = 0; i < distinctCount; i++) {
+    adjMatrix.push([]);
+    for (let j = 0; j < distinctCount; j++) {
+      adjMatrix[i].push(-1);
+    }
+  }
+  
+  //Now add the info from the text file to the matrix
+  for (let i = 0; i < parts.length; i++) {
+    const routeInfo = parts[i].split('');
+    const stopAIndex = routeInfo[0].charCodeAt(0) - 65;
+    const stopBIndex = routeInfo[1].charCodeAt(0) - 65;
+    const distance = parseInt(routeInfo[2]);
+    adjMatrix[stopAIndex][stopBIndex] = distance;
+  }
+  loaded = true;
+  console.log("Loaded :)")
+}
 
 //Takes a list of stops [A, B, C, D] etc and checks if there's a 
 //route between them checks length, if not prints "NO SUCH ROUTE"
 function getRouteLength(stopList) {
   
+  console.log("HERE");
+  console.log(adjMatrix);
+
+  let totalDistance = 0;
+
+  for (let i = 1; i < stopList.length; i++) {
+    const stopAIndex = stopList[i-1].charCodeAt(0) - 65;
+    const stopBIndex = stopList[i].charCodeAt(0) - 65;
+    const distance = adjMatrix[stopAIndex][stopBIndex];
+    if (distance == -1) {
+      console.log("NO SUCH ROUTE");
+      return;
+    }
+    totalDistance += distance;
+  }
+  return totalDistance;
 }
+
+parseFile('test.txt');
 
 
 //Finds the routes between the first and last stop with routes
